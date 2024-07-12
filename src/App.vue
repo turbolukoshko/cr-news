b53a1eb208eec0ab7dc739e9a7aa159c43930393
turbolukoshko <turbo.lukoshko@gmail.com>
2024-07-12 17:26:48 +0200

    created LatestArticles component and connected to the App

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import LatestArticles from './components/LatestArticles.vue'
+</script>
+
+<template>
+  <div id="app">
+    <LatestArticles />
+  </div>
+</template>
---
+++ src/components/LatestArticles.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="latest-updates">
+    <div class="filter-panel">
+      <label for="news">News </label>
+      <input type="checkbox" id="news" />
+      <label for="essays"> Essay</label>
+      <input type="checkbox" id="essays" />
+    </div>
+    <div class="article-box">
+      <ul></ul>
+    </div>
+  </div>
+</template>
